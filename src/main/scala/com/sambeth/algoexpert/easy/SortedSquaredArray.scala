b1d1b37c80b6e1e9df0d8396b8383f62e8b1aa82
sambeth <sambethslim@gmail.com>
2023-02-01 14:49:00 +0000

    add implementation for sorted squared array FP version

--- src/main/scala/com/sambeth/algoexpert/easy/SortedSquaredArray.scala
+++ src/main/scala/com/sambeth/algoexpert/easy/SortedSquaredArray.scala
@@ -1,5 +1,7 @@
 package com.sambeth.algoexpert.easy
 
+import scala.annotation.tailrec
+
 object SortedSquaredArray {
 
   private def sortedSquaredArrayUsingSorted(array: List[Int]): List[Int] =
@@ -25,12 +27,31 @@ object SortedSquaredArray {
     sortedArray.toList
 
   private def sortedSquaredArrayFP(array: List[Int]): List[Int] =
-    ???
+    val sortedArray: List[Int] = Array.fill(array.length)(0).toList
+
+    @tailrec
+    def loopThroughArray(array: List[Int], sortedArray: List[Int], arrayIndex: Int, startIndex: Int, endIndex: Int): List[Int] =
+      array match
+        case Nil => sortedArray
+        case head :: Nil =>
+          sortedArray.updated(arrayIndex, head * head)
+        case head :: tail if (Math.abs(head) < Math.abs(tail.last) || Math.abs(head) == Math.abs(tail.last)) =>
+          loopThroughArray(array.dropRight(1), sortedArray.updated(arrayIndex, tail.last * tail.last), arrayIndex - 1, startIndex, endIndex - 1)
+        case head :: tail if Math.abs(head) > Math.abs(tail.last) =>
+          loopThroughArray(array.drop(1), sortedArray.updated(arrayIndex, head * head), arrayIndex - 1, startIndex + 1, endIndex)
+
+    loopThroughArray(array, sortedArray, array.length - 1, 0, array.length - 1)
 
 
   @main def mainThree: Unit =
     println(sortedSquaredArrayUsingSorted(List(1,2,3,5,6,8,9)))
     println(sortedSquaredArrayNotUsingSorted(List(-2, -1)))
     println(sortedSquaredArrayNotUsingSorted(List(-5, -4, -3, -2, -1)))
+    println(sortedSquaredArrayFP(List(1, 2, 3, 5, 6, 8, 9)))
+    println(sortedSquaredArrayFP(List(-2, -1)))
+    println(sortedSquaredArrayFP(List(-5, -4, -3, -2, -1)))
+    println(sortedSquaredArrayFP(List(-5, -4, -3, -2, -1, 0, 1, 2, 3)))
+    println(sortedSquaredArrayFP(List(-2, -1, 0, 1, 2, 3, 4, 5)))
+    println(sortedSquaredArrayNotUsingSorted(List(-2, -1, 0, 1, 2, 3, 4, 5)))
 
 }
